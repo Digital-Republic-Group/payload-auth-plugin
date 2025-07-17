36b32db0968f12ed88150666bb54d918a3f22a16
= <will@digital-republic.co.uk>
2025-07-17 16:24:31 +0100

    run build

--- dist/esm/client/oauth.js
+++ dist/esm/client/oauth.js
@@ -1,6 +1,6 @@
 // src/client/oauth.ts
 var oauth = (options, provider) => {
-  const oauthURL = `${options.baseURL}/api/${options.name}/oauth/authorization/${provider}${options.redirectUri ? `?redirect_uri=${encodeURIComponent(options.redirectUri)}` : ""}`;
+  const oauthURL = `${options.baseURL}/api/${options.name}/oauth/authorization/${provider}?asdasd=adsasd&${options.redirectUri ? `redirect_uri=${encodeURIComponent(options.redirectUri)}` : ""}`;
   window.location.href = oauthURL;
 };
 export {
